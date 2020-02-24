031c4795cd1030ab9a5cdb10b06487c1cc5a7b3a
sithiralokuge <sanjithalokuge@gmail.com>
2020-02-24 18:06:26 +0530

    first commit

---
+++ app/src/main/java/com/example/testimage/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.testimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "TestingOK", Toast.LENGTH_LONG).show();
+    }
+}
